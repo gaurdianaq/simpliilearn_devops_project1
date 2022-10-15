4d937a7cebffda32a2a4dbd0226139ceebfb7e07
Evan Tatay-Hinds <evan.tatay-hinds@applydigital.co>
2022-10-15 13:06:01 -0400

    Updated setup script to have different flag options

--- setup.sh
+++ setup.sh
@@ -6,16 +6,32 @@
 export AWS_ACCESS_KEY_ID=<YOURKEY>
 export AWS_SECRET_ACCESS_KEY=<YOURKEY>
 
-public_ssh_key = <YOURPUBLICSSHKEY>
+public_ssh_key=<YOURKEY>
 
-cd ./packer
-packer init jenkins.pkr.hcl
-packer build jenkins.pkr.hcl
-if [ $? -eq 0 ]; then
-    cd ../terraform
-    terraform init
-    terraform apply
+if [ $1 == "full" ]; then #run packer and terraform (this will rebuild the ami and redeploy it, even if there are no changes)
+    cd ./packer
+    packer build jenkins.pkr.hcl
+    if [ $? -eq 0 ]; then
+        cd ../terraform
+        terraform apply -var "ssh_key=$public_ssh_key"
+    else
+        echo "Packer failed to build the AMI"
+    fi
+elif [ $1 == "terraform" ]; then #just run any terraform changes without running packer
+    cd terraform
+    terraform apply -var "ssh_key=$public_ssh_key"
+elif [ $1 == "initial" ]; then #runs the init command for terraform and packer, use on first run
+    cd ./packer
+    packer init jenkins.pkr.hcl
+    packer build jenkins.pkr.hcl
+    if [ $? -eq 0 ]; then
+        cd ../terraform
+        terraform init
+        terraform apply -var "ssh_key=$public_ssh_key"
+    else
+        echo "Packer failed to build the AMI"
+    fi
 else
-   echo "Packer failed to build the AMI"
+    echo $1 " is not a valid command, use 'full', 'terraform', or 'initial'"
 fi
 
