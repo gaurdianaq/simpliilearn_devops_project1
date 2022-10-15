bc1779b2947f21ff68d2a5fb84f841306f1523ba
Evan Tatay-Hinds <evan.tatay-hinds@applydigital.co>
2022-10-15 10:09:27 -0400

    Removed secrets, updated playbook.yml

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 #substitute your AWS keys here
 #execute this in the same folder as the script
 
-export AWS_ACCESS_KEY_ID=AKIAYCHWKF7STU67TQGJ
-export AWS_SECRET_ACCESS_KEY=2gKdr2bPGI3nh68WF5Ina6GNMoJ61BpnTHd3inUW
+export AWS_ACCESS_KEY_ID=<YOURKEY>
+export AWS_SECRET_ACCESS_KEY=<YOURKEY>
 
 cd ./packer
 packer init jenkins.pkr.hcl
